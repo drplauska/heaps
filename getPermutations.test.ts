347d454bdf5178f7bebfc5adb6322c7ecb5b5185
Ernestas Plauska <Ernestas_Plauska@epam.com>
2022-05-12 21:49:00 +0300

    add testing for permutations

--- getPermutations.test.ts
+++ getPermutations.test.ts
@@ -16,4 +16,22 @@ describe("getPermutations", () => {
       factorialize(thirdArray.length)
     );
   });
+  it("generates correct permutations from first array", () => {
+    const permutations = getPermutations(firstArray);
+    expect(permutations).toContainEqual([1, 2, 3]);
+    expect(permutations).toContainEqual([2, 1, 3]);
+    expect(permutations).toContainEqual([3, 1, 2]);
+    expect(permutations).toContainEqual([1, 3, 2]);
+    expect(permutations).toContainEqual([2, 3, 1]);
+    expect(permutations).toContainEqual([3, 2, 1]);
+  });
+  it("generates at least some correct permutations from second array", () => {
+    const permutations = getPermutations(secondArray);
+    expect(permutations).toContainEqual([1, 2, 3, 4, 5]);
+    expect(permutations).toContainEqual([2, 1, 3, 4, 5]);
+    expect(permutations).toContainEqual([3, 1, 2, 4, 5]);
+    expect(permutations).toContainEqual([1, 3, 2, 4, 5]);
+    expect(permutations).toContainEqual([2, 3, 1, 4, 5]);
+    expect(permutations).toContainEqual([3, 2, 1, 4, 5]);
+  });
 });
